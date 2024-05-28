180e61f4dfe4e37fa1b10dbda5ac1e62c7c8e3c3
guvi1dinesh <149289110+guvi1dinesh@users.noreply.github.com>
2024-05-28 06:50:16 -0400

    Update Task9.py

--- Task9.py
+++ Task9.py
@@ -11,34 +11,41 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webriver.firefox.service import Service
+from webdrivermanager.firefox import Geckodrivermanager
 
 class Secret_sauce:
     def sauce_demo:
-        url = 'https://www.saucedemo.com/'
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-        driver.get(url)
-        sleep(3)
-        print(driver.title)
-        sleep(3)
-        print(driver.current_url)
-        
-        element_of_name = driver.find_element(By.ID, "user-name")
-        element_of_name.send_keys("standard_user")
-        
-        element_of_first_password = driver.find_element(By.ID, "password")
-        element_of_first_password.send_keys("secret_sauce")
-        
-        webdriver_path = "C:\bin"
-        driver.get(url)
-        extract_source = driver.page_source
-        
-        with open('Webpage_task_11.txt', 'w', encoding='utf-8') as file:
-            file.write(extract_source)
-        
-        driver.quit()
+       try:
+             url = 'https://www.saucedemo.com/'
+             driver = webdriver.Firefox(service=service(GeckoDriverManager().install()))
+             driver.maximize_window()
+             driver.get(url)
+             sleep(3)
+             print(driver.title)
+             sleep(3)
+             print(driver.current_url)
+             
+             element_of_name = driver.find_element(By.ID, "user-name")
+             element_of_name.send_keys("standard_user")
+             
+             element_of_first_password = driver.find_element(By.ID, "password")
+             element_of_first_password.send_keys("secret_sauce")
+             
+             webdriver_path = "C:\bin"
+             driver.get(url)
+             extract_source = driver.page_source
+             
+             with open('Webpage_task_11.txt', 'w', encoding='utf-8') as file:
+                 file.write(extract_source)
+             
+             driver.quit()
+ 
         
         print("Webpage is extracted in 'Webpage_task_11.txt':")
+      except NoSuchElementException:
+        print("Error")
+      
     
 
 result = Secret_sauce
